068cdcc37bde6954989c2eb9b8849a58ee2e55c5
ymzEmre <dev.yilmazemre@gmail.com>
2022-06-13 11:31:37 +0300

    Add keyup enter to search box

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -11,6 +11,7 @@
       name: "HomePage",
     });
   };
+
 </script>
 
 <template>
@@ -19,8 +20,12 @@
       <span class="text-4xl font-black text-red-800 cursor-pointer" @click="goToHome()">MOVIES</span>
       <div class="mb-3 xl:w-96">
         <div class="input-group relative flex items-stretch md:ml-24">
-          <input type="search" v-model="searchValue" class="min-w-0 w-full px-4 py-1.5 text-base font-normal text-white bg-gray-800 bg-clip-padding rounded transition ease-in-out m-0 focus:bg-gray-700 focus:outline-none" placeholder="Search movie" aria-label="Search" aria-describedby="button-addon3">
-          <button  @click="$emit('search-movie', searchValue)" type="button" class="px-6 py-2.5 bg-gray-900 text-white ml-1 font-medium text-xs leading-tight rounded shadow-md hover:bg-gray-900 hover:shadow-lg focus:bg-gray-900 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-900 active:shadow-lg transition duration-150 ease-in-out">Search</button>
+          <input 
+          type="search" v-model="searchValue" @keyup.enter="$emit('search-movie', searchValue)"
+          class="min-w-0 w-full px-4 py-1.5 text-base font-normal text-white bg-gray-800 bg-clip-padding rounded transition ease-in-out m-0 focus:bg-gray-700 focus:outline-none" placeholder="Search movie">
+          <button  
+          type="button" @click="$emit('search-movie', searchValue); test()"
+          class="px-6 py-2.5 bg-gray-900 text-white ml-1 font-medium text-xs leading-tight rounded shadow-md hover:bg-gray-900 hover:shadow-lg focus:bg-gray-900 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-900 active:shadow-lg transition duration-150 ease-in-out">Search</button>
         </div>
       </div>
     </div>
